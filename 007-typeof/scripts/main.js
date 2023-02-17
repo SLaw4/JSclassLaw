d1f9a9be00e63a802d718b699a44fc40db87bcb9
Serene <SLaw4@MY.SCCCD.EDU>
2023-02-16 20:31:30 -0800

    more week 4

---
+++ 007-typeof/scripts/main.js
@@ -0,0 +1,13 @@
+let hot = 53; 
+console.log("Hot is 53 which is typeof ", (typeof hot))
+
+hot = "dog"; 
+console.log("Hot is dog which is typeof ", (typeof hot))
+
+hot = true; 
+console.log("Hot is true which is typeof ", (typeof hot))
+
+hot = 53; 
+const result = hot + "1"; 
+console.log("result of 53 + 1 is typeof - ", (typeof result)) 
+ 
