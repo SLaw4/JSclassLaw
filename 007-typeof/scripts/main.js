55fe1de8f001e4d09d86d731b0139c0dff2eaed8
Serene <SLaw4@MY.SCCCD.EDU>
2023-02-26 13:02:55 -0800

    more on variables

--- 007-typeof/scripts/main.js
+++ 007-typeof/scripts/main.js
@@ -1,11 +1,23 @@
-let word = "hello";
-let num = 12;
-let just = true;
+let word = "12"
+let num = 12
+let just = true
+const myConst = 42
+console.log(num == word)
+console.log(num === word)
 
-console.log(word);
-console.log(num);
-console.log(just);
+let myVar = null
+console.log(`myVar is typeof ${typeof myVar}`)
 
-console.log(typeof word);
-console.log(typeof num);
-console.log(typeof just);
+console.log(word)
+console.log(num)
+console.log(just)
+
+console.log(typeof word)
+console.log(typeof num)
+console.log(typeof just)
+
+// don't use var
+//if (true) {
+//   const myConst = 43
+//  console.log(myConst)
+//}
