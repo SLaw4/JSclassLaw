f61bef30c7fbc80c5ddd0c4c5a8300569651dfb6
Serene <SLaw4@MY.SCCCD.EDU>
2023-02-26 16:11:57 -0800

    fun with functions

---
+++ 008-functions/main.js
@@ -0,0 +1,11 @@
+//Functions are fun! 
+
+function doMath(a, b) {
+    
+    console.log(`this is the result ${a + b}`)
+    console.log(`this is the result ${a - b}`)
+    console.log(`this is the result ${a * b}`)
+    console.log(`this is the result ${a / b}`)
+}
+
+doMath(10, 4)
