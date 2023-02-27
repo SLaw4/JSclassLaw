aa658286b4fc629a566a686320a0878c9111046d
Serene <SLaw4@MY.SCCCD.EDU>
2023-02-26 16:47:20 -0800

    my funtions

--- 008-functions/main.js
+++ 008-functions/main.js
@@ -9,6 +9,25 @@ function doMath(a, b) {
     // console.log(`this is the result ${a / b}`)
 }
 
-
 const result = doMath(10, 4)
 console.log(`this result was returned from the function ${result}`)
+
+
+
+function totalVol(x, y, z) {
+    let total = x * y * z
+    return total
+}
+
+const total = totalVol(2, 10, 5)
+console.log(`Total volume of the box is ${total}`)
+
+function areaOfCircle(x) {
+    const pi = 3.14
+    let radius = x
+    let area = pi * x * x
+    return area
+}
+
+const area = areaOfCircle(3)
+console.log(`The area of the circle is ${area}`)
