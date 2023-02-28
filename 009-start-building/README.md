6100762b57a5ce82ef6b43cd870e99a1339b61b0
Serene <SLaw4@MY.SCCCD.EDU>
2023-02-28 12:26:08 -0800

    starting the mileage program

---
+++ 009-start-building/README.md
@@ -0,0 +1,5 @@
+# Week 7
+
+# Are you using devTools
+
+Yes I am using devTools. I had only used it before to inspect certain elements in my HTML file but these lectures have taught me so much more on how to better utilize it. It's extremely helpful for understanding what a line of code is doing and how it is being executed as well as for debugging purposes.
--- 009-start-building/main.js
+++ 009-start-building/main.js
@@ -7,4 +7,26 @@ const updateDOM = (input) => {
     divEl.appendChild(p)
 }
 
-updateDOM('adding content')
+// const mileage = (miles, gallons, price) => {
+//     updateDOM(miles/gallons * price)
+// }
+
+// const myMPG = mileage(300, 10, 3.79)
+
+// ANOTHER WAY TO WRITE IT 
+    // const mileage = (miles, gallons, price) => (miles/gallons * price)
+    // updateDOM(mileage(300, 10, 3.50))
+
+// IF PRICE IS DELETED 
+    // const mileage = (miles, gallons, price) => (miles/gallons * price)
+    // updateDOM(mileage(300, 10,)) // result: input becomes NaN because price is undefined 
+
+// SOLUTION: SET A DEFAULT PRICE 
+    // const mileage = (miles, gallons, price = 3.79) => (miles/gallons * price)
+        
+    // updateDOM(mileage(300, 10,))
+
+// BUT IF YOU END UP PASSING A VALUE TO PRICE, IT WILL TAKE THAT FIRST 
+        const mileage = (miles, gallons, price = 3.79) => (miles/gallons * price)
+        
+        updateDOM(mileage(300, 10, 5.40)) // Would only take the value 3.79 if 5.40 was not there
