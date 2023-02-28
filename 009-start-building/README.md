5c3d604ef5545a2d1a036673087cb6148c811234
Serene <SLaw4@MY.SCCCD.EDU>
2023-02-28 12:57:12 -0800

    learning about arrays

--- 009-start-building/README.md
+++ 009-start-building/README.md
@@ -3,3 +3,7 @@
 # Are you using devTools
 
 Yes I am using devTools. I had only used it before to inspect certain elements in my HTML file but these lectures have taught me so much more on how to better utilize it. It's extremely helpful for understanding what a line of code is doing and how it is being executed as well as for debugging purposes.
+
+# Does the concept of arrays make sense to you?
+
+So I have taken a JavaScript course before that briefly covered arrays. My understanding is that it is basically a tool that stores data into a list. Please correct me if I'm wrong. This is where the content becomes confusing to me especially when loops are added into the mixture. I think providing a real life example of how arrays work would help me better understand its function. I do appreciate that the professor show step by step of what is going on and explaning what is happening. 
--- 009-start-building/main.js
+++ 009-start-building/main.js
@@ -1,11 +1,27 @@
 //Update the DOM
 
+const myArr = []
+
 const updateDOM = (input) => {
     const divEl = document.querySelector('#output')
     const p = document.createElement('p')
     p.textContent = input
     divEl.appendChild(p)
 }
+
+const trackMPGCost = (miles, gallons, price = 3.79) => {
+    const MPG = miles/gallons
+    const tripCost = MPG * price
+    myArr.push(MPG, tripCost)
+    myArr.splice(MPG, tripCost)
+}
+
+
+
+updateDOM(trackMPGCost(300, 10, 5.40))
+updateDOM(trackMPGCost(320, 12, 5))
+
+
 
 // const mileage = (miles, gallons, price) => {
 //     updateDOM(miles/gallons * price)
@@ -27,6 +43,6 @@ const updateDOM = (input) => {
     // updateDOM(mileage(300, 10,))
 
 // BUT IF YOU END UP PASSING A VALUE TO PRICE, IT WILL TAKE THAT FIRST 
-        const mileage = (miles, gallons, price = 3.79) => (miles/gallons * price)
+        // const mileage = (miles, gallons, price = 3.79) => (miles/gallons * price)
         
-        updateDOM(mileage(300, 10, 5.40)) // Would only take the value 3.79 if 5.40 was not there
+        // updateDOM(mileage(300, 10, 5.40)) Would only take the value 3.79 if 5.40 was not there
