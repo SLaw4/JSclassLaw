b0b0eb0457f08c4a13117f1e1213afa4b0673cf7
Serene <SLaw4@MY.SCCCD.EDU>
2023-03-22 12:35:15 -0700

    this is mine

--- 017-intro-to-objects/main.js
+++ 017-intro-to-objects/main.js
@@ -3,19 +3,43 @@ const myArr = []
 // console.log(typeof myObj)
 // console.log(typeof myArr)
 // console.log(myArr === myObj)
-const myNumber = 9 
-const myObj = {
-    name: 'Rio',
-    age: 59,
-    hobbies: ['coding', 'running', 'workout'],
-    mobile: {
-        make: 'pixel 7', 
-        OS: 'android',
+// const myNumber = 9 
+// const myObj = {
+//     name: 'Rio',
+//     age: 59,
+//     hobbies: ['coding', 'running', 'workout'],
+//     mobile: {
+//         make: 'pixel 7', 
+//         OS: 'android',
+//     },
+//     myGreeting: function(person) {
+//         return `${this.name} who loves to ${this.hobbies[0]} says What is UP ${person}`
+//     },
+// }
+
+// const greeting = myObj.myGreeting('jane')
+// console.log(greeting)
+const age = ""
+const mineObj = {
+    name: 'Serene',
+    age: 30,
+    height: 5.1,
+    car: {
+        make: 'Mazda',
+        model: 'CX-5',
     },
-    myGreeting: function(person) {
-        return `${this.name} who loves to ${this.hobbies[0]} says What is UP ${person}`
+    classes: ['HTML', 'JavaScript', 'CSS', 'Python'],
+
+    myStatement: function(person, age) {
+        if (age === 30) {
+            console.log(`${person} is the same age as ${this.name}`)
+        } else if (age > 30) {
+            console.log(`${person} is older than ${this.name}`)
+        } else {
+            console.log(`${person} is younger than ${this.name}`)
+        }
     },
 }
 
-const greeting = myObj.myGreeting('jane')
-console.log(greeting)
+const myHello = mineObj.myStatement('Lucy', 40)
+console.log(myHello)
