a79ff5a2e4ed309f11474effb4d281f11233e810
Serene <SLaw4@MY.SCCCD.EDU>
2023-03-28 17:31:34 -0700

    step 1 without working code

--- 024-modules/main.js
+++ 024-modules/main.js
@@ -1,8 +1,9 @@
+import {renderTable} from "./render.js";
 /* Global const for updating DOM elements by their id */
 const FORM = document.getElementById('form-input');
 const ERR = document.getElementById('err');
 const AVG_OUTPUT = document.getElementById('output-avg');
-const TBL_OUTPUT = document.getElementById('table-out');
+
 
 /* MY_DATA is global array that will be updated by the user with objects from form input values and calculate data */
 function getTripData() {
@@ -16,8 +17,8 @@ function getTripData() {
 function saveTripData() {
     localStorage.setItem('tripdata', JSON.stringify(MY_DATA))
 }
-const MY_DATA = getTripData()
-renderTable()
+const MY_DATA = getTripData();
+renderTable(MY_DATA)
 /* updateDOM function takes in input (string value) and id (to dertermine DOM location to update) and create and updates DOM elements */
 
 function updateDOM(input, id) {
@@ -73,61 +74,6 @@ function isFormValid(miles, gallons, price) {
     }
 }
 
-function renderTableHeadings() {
-    const tbl = document.createElement('table');
-    const headings = ['Miles Drive:', 'Gallons Used:', 'Price Paid:', 'Trip MPG', 'Trip Cost', 'Edit/Delete'];
-    const tr = document.createElement('tr');
-    headings.forEach(function (heading) {
-        let th = document.createElement('th');
-        th.textContent = heading;
-        tr.appendChild(th);
-    });
-    tbl.appendChild(tr);
-    return tbl;
-}
-function renderEditDelBtn(index) {
-    const td = document.createElement('td');
-    const editBtn = document.createElement('button');
-    editBtn.textContent = 'edit';
-    const delBtn = document.createElement('button');
-    delBtn.textContent = 'delete';
-    // add eventlistener 
-    // for edit, reference the obj in the array for the current row in the table and populate the form input fields
-    editBtn.addEventListener('click', function(e) {
-        FORM[0].value = MY_DATA[index].miles
-        FORM[1].value = MY_DATA[index].gallons
-        FORM[2].value = MY_DATA[index].price
-        MY_DATA.splice(index, 1)
-    })
-    delBtn.addEventListener('click', function(e){
-       MY_DATA.splice(index, 1)
-       saveTripData()
-       renderTable()
-    }) 
-    td.appendChild(editBtn);
-    td.appendChild(delBtn);
-    return td;
-
-}
-function renderTable() {
-    TBL_OUTPUT.innerHTML = '';
-    if(MY_DATA.length !== 0) {
-        const tbl = renderTableHeadings();
-        TBL_OUTPUT.appendChild(tbl);
-        MY_DATA.forEach(function (obj, index) {
-            const tr = document.createElement('tr');
-            for (key in obj) {
-                let td = document.createElement('td');
-                td.textContent = obj[key];
-                tr.appendChild(td);
-            }
-
-            const btnTD = renderEditDelBtn(index);
-            tr.appendChild(btnTD);
-            tbl.appendChild(tr);
-        });
-    }
-}
 
 /* Evenlisteners for form submit button, checks validation and if valid saves data as an object into global array name MY_DATA */
 
@@ -143,7 +89,7 @@ FORM.addEventListener('submit', (e) => {
         const dataObj = trackMPGandCost(miles, gallons, price);
         MY_DATA.push(dataObj);
         saveTripData()
-        renderTable();
+        renderTable(MY_DATA, FORM);
         calculateAvg();
     }
     FORM.reset();
