2f939e0b525ad35d1031717e375741721f49258c
Serene <SLaw4@MY.SCCCD.EDU>
2023-04-03 16:56:31 -0700

    intro to higher order functions

---
+++ 026-higher-order-functions/README.md
@@ -0,0 +1,4 @@
+# Week 12
+
+# Higher Order Functions video
+Higher order function takes a function as a parameter OR returns a function. A function takes a parameter usually to perform some kind of operation on it or with it. Example: using array.filter() vs writing a for loop. Using higher order functions helps to simplify code, making it easier to read and modify. Combining higher order functions with first order functions helps to modularize code. 
---
+++ 026-higher-order-functions/calculateandupdate.js
@@ -0,0 +1,58 @@
+const ERR = document.getElementById('err');
+
+/* updateDOM function takes in input (string value) and id (to dertermine DOM location to update) and create and updates DOM elements */
+function updateDOM(input, id) {
+    const divEl = document.querySelector(id);
+    const p = document.createElement('p');
+    p.textContent = input;
+    divEl.appendChild(p);
+}
+
+/* trackMPGandCost function takes in miles, gallons and price and calculates MPG and tripCost and returns an object */
+
+function trackMPGandCost(miles, gallons, price) {
+    const MPG = Number((miles / gallons).toFixed(2));
+    const tripCost = Number((gallons * price).toFixed(2));
+    return {
+        miles: miles,
+        gallons: gallons,
+        price: price,
+        MPG: MPG,
+        tripCost: tripCost,
+    };
+}
+
+/* calculateAve function loops over the MY_DATA to determine average MPG and Trip Cost */
+
+function calculateAvg(MY_DATA) {
+    const numberOfObj = MY_DATA.length;
+    let sumMPG = 0;
+    let sumTripCost = 0;
+    MY_DATA.forEach(obj => {
+        sumMPG += obj.MPG;
+        sumTripCost += obj.tripCost;
+    });
+    const avgMPG = Number((sumMPG / numberOfObj).toFixed(2));
+    const avgTripCost = Number((sumTripCost / numberOfObj).toFixed(2));
+    updateDOM(`Average MPG is ${avgMPG}`, '#output-avg');
+    updateDOM(`Average trip cost is ${avgTripCost}`, '#output-avg');
+}
+
+/* isFormValid takes in miles, gallons, and price and does simple validation and returns boolean or truthy value back to eventlisteners */
+function isFormValid(miles, gallons, price) {
+    const errMsg = [];
+    if (miles === 0 || gallons === 0 || price === 0) {
+        errMsg.push('Make sure you input value greater than 0');
+    }
+    if (price > 1000) {
+        errMsg.push('Really? I think this is an error...try again');
+    }
+    if (errMsg.length > 0) {
+        ERR.textContent = errMsg;
+
+    } else {
+        return true;
+    }
+}
+
+export {trackMPGandCost, calculateAvg, updateDOM, isFormValid}
