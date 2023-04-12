ac77bdb78174922dc14a16f5305a58b1bcb7dcec
Serene <SLaw4@MY.SCCCD.EDU>
2023-04-12 15:38:33 -0700

    student subclass

--- 027-intro-to-oop/README.md
+++ 027-intro-to-oop/README.md
@@ -2,3 +2,6 @@
 
 # Class syntax difference 
 So because we were somewhat exposed to writing objects, this object oriented programming syntax isn't too complicated to understand. I feel like the methods are just rearranged and the syntax is slightly different at least so far.
+
+# Prototype chain and student subclass
+I can understand the basic logic of these two concepts. Where prototype chain is demonstrating inheritence and student subclass extends the main class. 
--- 027-intro-to-oop/main.js
+++ 027-intro-to-oop/main.js
@@ -9,7 +9,17 @@ class Person {
   }
 }
 
-const rio = new Person("Rio", "Waller", "123 ABC street");
+class Student extends Person {
+    constructor(firstName, lastName, address, classList) {
+        super(firstName, lastName, address)
+        this.classList = classList
+    }
+    showStatus() {
+        console.log(`${this.firstName} lives at ${this.address} is taking ${this.classList}`)
+    }
+}
+
+const rio = new Student("Rio", "Waller", "123 ABC street", ["ENG1A", "CIT93"]);
 rio.showStatus()
 const jane = new Person("Jane", "Doe", "456 DEF street")
 jane.showStatus()
