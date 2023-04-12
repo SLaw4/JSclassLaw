6ca41baf3ed1fbe38fd50c10c58ae9dffd747d13
Serene <SLaw4@MY.SCCCD.EDU>
2023-04-12 15:54:26 -0700

    my attempt at oop coding challenge

--- 027-intro-to-oop/main.js
+++ 027-intro-to-oop/main.js
@@ -1,11 +1,29 @@
 class Person {
   constructor(firstName, lastName, address) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.address = address;
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._address = address;
+  }
+  getFirstName() {
+    return `${this._firstName}`
+  }
+  setFirstName(newFirstName) {
+    this._firstName = newFirstName
+  }
+  getLastName() {
+    return `${this._lastName}`
+  }
+  setLastName(newLastName) {
+    this._lastName = newLastName
+  }
+  getAddress() {
+    return `${this._address}`
+  }
+  setAddress(newAddress) {
+    this._address = newAddress
   }
   showStatus() {
-    console.log(`${this.firstName} lives at ${this.address}`)
+    console.log(`${this.getFirstName()} lives at ${this._address}`)
   }
 }
 
@@ -14,8 +32,14 @@ class Student extends Person {
         super(firstName, lastName, address)
         this.classList = classList
     }
+    getClassList () {
+        return `${this.classList}`
+    }
+    setClassList(newClassList) {
+        this.classList = newClassList
+    }
     showStatus() {
-        console.log(`${this.firstName} lives at ${this.address} is taking ${this.classList}`)
+        console.log(`${this._firstName} lives at ${this._address} is taking ${this.classList}`)
     }
 }
 
@@ -23,3 +47,5 @@ const rio = new Student("Rio", "Waller", "123 ABC street", ["ENG1A", "CIT93"]);
 rio.showStatus()
 const jane = new Person("Jane", "Doe", "456 DEF street")
 jane.showStatus()
+
+console.log(rio.getFirstName())
