2cb913672ad69d87bde7fd77f2fca1469cfe0c50
Serene <SLaw4@MY.SCCCD.EDU>
2023-04-19 13:32:42 -0700

    async code

--- 029-async-code/main.js
+++ 029-async-code/main.js
@@ -1,12 +1,36 @@
-let pizza
-function orderPizza() {
-    console.log('Order pizza')
-    setTimeout(() => {
-        pizza = `🍕`
-        console.log(`${pizza} is ready`)
-    }, 2000)
-    console.log('Pizza was ordered')
+// function orderPizza(callback) {
+//     setTimeout(() => {
+//         const pizza = `🍕`
+//         callback(pizza)
+//     }, 2000);
+// }
+
+// function pizzaReady(pizza) {
+//     console.log(`Eat the ${pizza}`)
+// }
+// orderPizza(pizzaReady)
+// console.log(`Call friend`)
+
+// function thing1() {
+//     //Call pizza shop
+// }
+// function thing2() {
+//     //Order the pizza
+// }
+// function thing3() {
+//     //Eat the pizza
+// }
+function thing1(callback) {
+    callback()
 }
-orderPizza()
-console.log('Call friend')
-console.log(`Eat ${pizza}`)
+function thing2(callback) {
+    callback()
+}
+function thing3(callback) {
+    callback()
+}
+thing1(()=> {
+    thing2(() => {
+        thing3()
+    })
+})
