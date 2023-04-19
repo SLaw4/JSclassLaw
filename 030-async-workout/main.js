9eae1d2c1e93f8e4bbdb05bf37cb9e050c1a42b7
Serene <SLaw4@MY.SCCCD.EDU>
2023-04-19 15:28:14 -0700

    my code

--- 030-async-workout/main.js
+++ 030-async-workout/main.js
@@ -1,10 +1,28 @@
 const formEL = document.getElementById('form')
 const output = document.getElementById('output')
 
+function updateDOM(input) {
+    const divEl = document.querySelector('#output')
+    const p = document.createElement('p')
+    p.textContent = input
+    divEl.appendChild(p)
+}
+
+function getInput(callback) {
+    const getExercise = document.getElementById('type').value
+    const getReps = document.getElementById('reps').value
+    updateDOM(`${getExercise} for ${getReps} reps`)
+    setTimeout (() => {
+        const getTime = document.getElementById('time').value
+        callback(getTime)
+    }, 2000)
+}
+
 formEL.addEventListener('submit', function(e) {
     e.preventDefault();
     const type = e.target.type.value
     const reps = parseFloat(e.target.reps.value)
     const time = parseFloat(e.target.time.value)
+    getInput()
     formEL.reset()
 })
