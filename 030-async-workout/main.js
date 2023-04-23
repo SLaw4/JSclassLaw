a16b946932cddd9762188d8c2fe73be002b4c907
Serene <SLaw4@MY.SCCCD.EDU>
2023-04-23 13:49:33 -0700

    fixing promise code

--- 030-async-workout/main.js
+++ 030-async-workout/main.js
@@ -8,25 +8,27 @@ function updateDOM(message, el) {
 }
 
 function startWorkout(type, reps, time, fn) {
-    return new Promise(function() {
     fn(`Start ${type} <> Goal reps is ${reps}`, 'p')
-    setTimeout(() => {
-        fn(`Stop ${type}`, 'h1')
-    }, time * 1000)
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            //resolve(fn(`Stop ${type}`, 'h1'))
+            reject(fn('Something bad happened', 'h1'))
+        }, time * 1000)
     })
 }
 
-
+function onError(err) {
+    console.log(`ERROR ${err}`)
+}
 formEL.addEventListener('submit', function(e) {
     e.preventDefault();
     const type = e.target.type.value
     const reps = parseFloat(e.target.reps.value)
     const time = parseFloat(e.target.time.value)
-    startWorkout(type, reps, time, updateDOM)
+    startWorkout(type, reps, time, updateDOM).then().catch(onError)
     formEL.reset()
 })
 
-startWorkout()
-    .then(updateDOM)
-    .finally(formEL.addEventListener)
+
+
     
