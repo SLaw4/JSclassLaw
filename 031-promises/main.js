b05d10abbff61c2b51b7ef67bb33ce879a2fe289
Serene <SLaw4@MY.SCCCD.EDU>
2023-04-23 12:48:43 -0700

    intro to promises

---
+++ 031-promises/main.js
@@ -0,0 +1,38 @@
+function getWeather() {
+    return new Promise(function(resolve, reject) {
+       setTimeout(() => {
+        resolve('Sunny')
+       }, 100)
+    })
+}
+
+function getWeatherIcon(weather) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+         switch(weather) {
+            case 'Sunny':
+                resolve('☀')
+                break
+            case 'Cloudy':
+                resolve('☁')
+                break
+            case 'Rainy':
+                resolve('🌧')
+                break  
+            default:
+                reject('NO ICON FOUND')                  
+         }
+        }, 100)
+     })
+}
+
+function onSuccess(data) {
+    console.log(`Success ${data}`)
+}
+
+function onError(error) {
+    console.log(`Error: ${error}`)
+}
+getWeather()
+    .then(getWeatherIcon)
+    .then(onSuccess, onError)
