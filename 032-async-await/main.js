139060a1c2ea96da052bfbedbac8c807f5ad37a5
Serene <SLaw4@MY.SCCCD.EDU>
2023-04-25 11:16:08 -0700

    try/catch

--- 032-async-await/main.js
+++ 032-async-await/main.js
@@ -15,22 +15,58 @@
 //             console.log(result)
 //         })
 // }
-async function start() {
-    const data = await fetch('https://api.weather.gov/gridpoints/HNX/53,100/forecast')
-    const result = await data.json()
-    console.log(result.properties.periods[1].shortForecast)
-}
+// async function start() {
+//     const data = await fetch('https://api.weather.gov/gridpoints/HNX/53,100/forecast')
+//     const result = await data.json()
+//     console.log(result.properties.periods[1].shortForecast)
+// }
 
-async function start2() {
-    fetch('https://api.weather.gov/gridpoints/HNX/53,100/forecast')
-    .then(data => data.json())
-    .then(result => {
-        console.log(result.properties.periods[1].shortForecast)
+// async function start2() {
+//     fetch('https://api.weather.gov/gridpoints/HNX/53,100/forecast')
+//     .then(data => data.json())
+//     .then(result => {
+//         console.log(result.properties.periods[1].shortForecast)
+//     })
+// }
+
+// start()
+
+function getData() {
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            resolve('Here is your data!')
+        }, 1)
     })
 }
 
-start()
+// function onSuccess() {
 
+// }
+
+// function onFailure() {
+
+// }
+async function start() {
+    try {
+        const result = await getData()
+        //SUCCCESS
+    } catch (error) {
+        //FAILURE
+    }
+} 
+
+// async function start2() {
+//     const result = await getData()
+//     .catch(error => {
+//         console.log(`ERROR: ${error}`)
+//     })
+//     console.log(result)
+// } 
+start()
 // 1. async and await must be used together
 //     exceptions: JS modules & Chrome DevTools Console
-// 2. async/
+// 2. async/await only affects Promise receiver 
+// 3. You can await any function that returns a Promise 
+// 4. Any function can be converted to async
+// 5. All async functions return a promise
+// 6. Error handling with try/catch
