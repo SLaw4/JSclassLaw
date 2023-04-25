4962d44385ef33939d538a550c9a7c2d1614f1e4
Serene <SLaw4@MY.SCCCD.EDU>
2023-04-25 11:01:41 -0700

    actual example

--- 032-async-await/main.js
+++ 032-async-await/main.js
@@ -1,33 +1,33 @@
-function getData() {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve(46)
-        },1)
-    })
-}
-async function start() {
-    const result = await getData()
-    console.log(result)
-}
+// function getData() {
+//     return new Promise(resolve => {
+//         setTimeout(() => {
+//             resolve(46)
+//         },1)
+//     })
+// }
+// async function start() {
+//     const result = await getData()
+//     console.log(result)
+// }
 // async function start2() {
 //     getData()
 //         .then(result => {
 //             console.log(result)
 //         })
 // }
-// async function start() {
-//     const data = await fetch('https://api.weather.gov/gridpoints/HNX/53,100/forecast')
-//     const result = await data.json()
-//     console.log(result.properties.periods[1].shortForecast)
-// }
+async function start() {
+    const data = await fetch('https://api.weather.gov/gridpoints/HNX/53,100/forecast')
+    const result = await data.json()
+    console.log(result.properties.periods[1].shortForecast)
+}
 
-// async function start2() {
-//     fetch('https://api.weather.gov/gridpoints/HNX/53,100/forecast')
-//     .then(data => data.json())
-//     .then(result => {
-//         console.log(result.properties.periods[1].shortForecast)
-//     })
-// }
+async function start2() {
+    fetch('https://api.weather.gov/gridpoints/HNX/53,100/forecast')
+    .then(data => data.json())
+    .then(result => {
+        console.log(result.properties.periods[1].shortForecast)
+    })
+}
 
 start()
 
