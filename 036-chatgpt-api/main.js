eb1890f111421325257298020edfc84c71f28a73
Serene <SLaw4@MY.SCCCD.EDU>
2023-05-05 17:59:02 -0700

    my thoughts

--- 036-chatgpt-api/main.js
+++ 036-chatgpt-api/main.js
@@ -1,11 +1,16 @@
 import {KEY} from "./key.js"
 
-const options = new Headers({
-    Headers: {
-        'Authorization': 'Bearer',
-        'Content-Type': 'application/json'
-    }
-})
+const options = {
+    method: 'POST',
+    headers: {
+        'Authorization': `Bearer ${KEY}`,
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        'model': 'gpt-3.5-turbo',
+        'messages': [{'role': 'user', 'content': 'Can you teach me JS?'}]
+    })
+}
 
 fetch('https://api.openai.com/v1/completions', options)
     .then(response => response.json())
