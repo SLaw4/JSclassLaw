69944b278ed87ccafc6c089191d22ca7c8c2478e
Serene <SLaw4@MY.SCCCD.EDU>
2023-05-15 18:27:43 -0700

    array types

--- 036-typescript/build/js/main.js
+++ 036-typescript/build/js/main.js
@@ -1,15 +1,11 @@
 "use strict";
-let myName = 'Dave';
-let meaningOfLife;
-let isLoading;
-let album;
-myName = 'John';
-meaningOfLife = 42;
-isLoading = true;
-album = 5150;
-const sum = (a, b) => {
-    return a + b;
-};
-let postId;
-let isActive;
-let re = /\w+/g;
+let stringArr = ['one', 'hey', 'Dave'];
+let guitars = ['Strat', 'Les Paul', 5150];
+let mixedData = ['EVH', 1984, true];
+stringArr[0] = 'John';
+stringArr.push('hey');
+guitars[0] = 1984;
+guitars.unshift('Jim');
+let test = [];
+let bands = [];
+bands.push('Van Halen');
--- 036-typescript/src/main.ts
+++ 036-typescript/src/main.ts
@@ -1,18 +1,15 @@
-let myName: string = 'Dave'
-let meaningOfLife: number;
-let isLoading: boolean;
-let album: any;
+let stringArr = ['one', 'hey', 'Dave']
 
-myName = 'John'
-meaningOfLife = 42
-isLoading = true
-album = 5150
+let guitars = ['Strat', 'Les Paul', 5150]
 
-const sum = (a: number, b: string) => {
-    return a + b
-}
+let mixedData = ['EVH', 1984, true]
 
-let postId: string | number 
-let isActive: number | boolean
+stringArr[0] = 'John'
+stringArr.push('hey')
 
-let re: RegExp = /\w+/g
+guitars[0] = 1984
+guitars.unshift('Jim')
+
+let test = []
+let bands: string[] = []
+bands.push('Van Halen')
